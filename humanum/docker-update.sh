94459c4e80e4c681c4642e089b0a31cf49b23275
Peter Stadler <stadlerpeter@yahoo.fr>
2019-11-19 09:43:48 +0100

    add CONTAINERs variable to be certain about removal
    of services.

--- humanum/docker-update.sh
+++ humanum/docker-update.sh
@@ -5,20 +5,24 @@
 #!/bin/bash
 
 IMAGES=()
+CONTAINERS=()
 COMPOSE_FILE=""
 SERVICE=$1
 
 case $SERVICE in
     roma)
     IMAGES=("teic/xquery4roma" "teic/roma")
+    CONTAINERS=("xquery4roma" "roma")
     COMPOSE_FILE=docker-compose_roma.yml
     ;;
     romabeta)
     IMAGES=("teic/romajs")
+    CONTAINERS=("romabeta")
     COMPOSE_FILE=docker-compose_romabeta.yml
     ;;
     oxgarage)
     IMAGES=("teic/oxgarage")
+    CONTAINERS=("oxgarage")
     COMPOSE_FILE=docker-compose_oxgarage.yml
     ;;
     *)
@@ -29,7 +33,7 @@ esac
 
 echo "*************************"
 echo stopping service $SERVICE
-docker-compose -f $COMPOSE_FILE down
+for container in ${CONTAINERS[@]} ; do docker kill $container ; done
 
 echo "*************************"
 echo pulling images
